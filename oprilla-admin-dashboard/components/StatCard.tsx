37edbe374c16898f8b65199813338d110e89a15e
harshitharai-888 <harshithar@ccsym.com>
2026-06-09 15:51:45 +0530

    Added stat card component

---
+++ oprilla-admin-dashboard/components/StatCard.tsx
@@ -0,0 +1,19 @@
+type StatCardProps = {
+  title: string;
+  value: string;
+};
+
+export default function StatCard({
+  title,
+  value,
+}: StatCardProps) {
+  return (
+    <div className="bg-white rounded-lg shadow p-6 w-60">
+      <h3 className="text-gray-500 text-sm">{title}</h3>
+
+      <p className="text-3xl font-bold mt-2">
+        {value}
+      </p>
+    </div>
+  );
+}

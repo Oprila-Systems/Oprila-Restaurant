907fa77aba79bbf4949a796751eda3ba8a5613a7
harshitharai-888 <harshithar@ccsym.com>
2026-06-18 17:40:17 +0530

    Add reusable ReservationRow component

--- oprilla-admin-dashboard/app/dashboard/bookings/ReservationRow.tsx
+++ oprilla-admin-dashboard/app/dashboard/bookings/ReservationRow.tsx
@@ -1,28 +1,23 @@
 type Props = {
-  customer: string;
-  guests: number;
-  table: string;
-  status: string;
-  source: string;
-  time: string;
+  item: {
+    customer: string;
+    guests: number;
+    table: string;
+    status: string;
+    source: string;
+    time: string;
+  };
 };
 
-export default function ReservationRow({
-  customer,
-  guests,
-  table,
-  status,
-  source,
-  time,
-}: Props) {
+export default function ReservationRow({ item }: Props) {
   return (
     <tr className="border-b">
-      <td className="p-4">{customer}</td>
-      <td className="p-4">{time}</td>
-      <td className="p-4">{guests}</td>
-      <td className="p-4">{table}</td>
-      <td className="p-4">{status}</td>
-      <td className="p-4">{source}</td>
+      <td className="p-4">{item.customer}</td>
+      <td className="p-4">{item.time}</td>
+      <td className="p-4">{item.guests}</td>
+      <td className="p-4">{item.table}</td>
+      <td className="p-4">{item.status}</td>
+      <td className="p-4">{item.source}</td>
     </tr>
   );
 }

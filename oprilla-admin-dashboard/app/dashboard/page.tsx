3f44709e3e9fcf37b4405adbb108b01b16aa344a
harshitharai-888 <harshithar@ccsym.com>
2026-06-30 20:22:34 +0530

    Disable body scroll when mobile sidebar is open

--- oprilla-admin-dashboard/app/dashboard/page.tsx
+++ oprilla-admin-dashboard/app/dashboard/page.tsx
@@ -5,30 +5,38 @@ import { useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
+import { useBodyScrollLock } from "./hooks/useBodyScrollLock";
 
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  useBodyScrollLock(sidebarOpen);
+
   return (
     <div className="min-h-screen flex bg-[#F9F8F4]">
+      {/* Desktop Sidebar */}
       <div className="hidden lg:block">
         <Sidebar />
       </div>
 
+      {/* Mobile Sidebar */}
       {sidebarOpen && (
         <>
+          {/* Overlay */}
           <div
             className="fixed inset-0 z-40 bg-black/40 lg:hidden"
             onClick={() => setSidebarOpen(false)}
           />
 
-          <div className="fixed left-0 top-0 z-50 h-full lg:hidden">
+          {/* Sidebar */}
+          <div className="fixed inset-y-0 left-0 z-50 w-[280px] overflow-y-auto lg:hidden">
             <Sidebar />
           </div>
         </>
       )}
 
-      <main className="flex-1 overflow-x-auto bg-[#F9F8F4]">
+      {/* Main Content */}
+      <main className="flex-1 overflow-x-hidden bg-[#F9F8F4]">
         <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
 
         <Dashboard />
